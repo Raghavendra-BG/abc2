1ec521a4f020c57ddf6033bfda4cc01e71385cac
poojaprakash49 <poojaprakash49@gmail.com>
2016-07-20 12:53:34 +0530

    java servlet program

---
+++ Abc/src/com/src/view/Hello.java
@@ -0,0 +1,53 @@
+package com.src.view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out=response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<form action='Display.view' method='post'>");
+		out.println("Email");
+		out.println("<input type='text' name='email'> ");
+		out.println("Password<input type='password' name='password'> ");
+		out.println("<input type='submit' value='login'>");
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
